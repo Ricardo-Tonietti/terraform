e62ad3a9b2bf7955ee4e1238833d30664c9832b4
ricardo tonietti <rictonietti@gmail.com>
2023-07-19 18:58:05 -0300

    ajust file

--- main.tf
+++ main.tf
@@ -10,23 +10,16 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
-  subnet_id = "subnet-0703c6eb7658c743d"
-  key_name = "iac-rictonietti"
-  user_data = <<-EOF
-            #!/bin/bash
-            cd /home/ubuntu
-            echo "<h1>Feito com terraform</h1>" > index.html
-            nohup busybox httpd -f -p 8080 &
-            EOF
-
-
+  subnet_id     = "subnet-0703c6eb7658c743d"
+  key_name      = "iac-rictonietti"
+  
   tags = {
-    Name = "teste AWS"
+    Name = "EC2 - Terraform"
   }
 }
